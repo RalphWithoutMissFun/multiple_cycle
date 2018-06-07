8af60aba21a9743802387235db2d490eb92a3a36
e <ralphshaides@gmail.com>
2018-06-08 02:11:48 +0800

    uncomment

--- compiler/convert.cpp
+++ compiler/convert.cpp
@@ -13,7 +13,6 @@ map<string, string> binary_convert;
 int map_len = 32;
 int map_len2 = 17;
 void print_vec(vector<int> v){
-	//cout << v.size();
   int len = v.size();
 	for (int index =0 ; index < len ; index++){
 		cout << v[index];
@@ -21,22 +20,9 @@ void print_vec(vector<int> v){
 		 cout << " ";
 	}
 	cout << endl;
-	/*
-	vector<int>::iterator ite = v.begin();
-	for (ite ; ite != v.end() ; ite++)
-		cout << *ite ;
-	cout << endl;
-	*/
+
 }
 
-/*
-  * read from a file named code.txt and output the corresponding
-  * machine code to the file convert.txt
-  * use the basic knowledge of map
-  */
-/*
- * part 0 , read the map data and store it in the map
- */
 void initialize(){
    fstream file ;
    file.open("map_data.txt");
@@ -60,29 +46,18 @@ void initialize(){
    }
    file.close();
 }
-/*
- * part1 : convert the opcode to the binary to vector of binary code
- * 1 and 0
- * and use the least byte to signal the type of the instruction
- * R, S and I
- */
 vector<int> convert_opcode(string target, int len){
 	vector<int> opcode;
 	for (int index = 0 ; index < len; index++)
 		opcode.push_back(0);
   string str = data[target];
-	//cout << target << "----" << endl;
   int leng = str.size();
-	//cout << "debug " << leng << "  " << str << endl;
   for (int index = 0 ; index < leng ; index++)
   {
   	opcode[index] = str[index] - '0';
   }
     return opcode;
 }
-/*
- * part2 : convert the number to given buyes
- */
 vector<int> convert_num(string str, int bytes, int zero_sign){
 
 	if (str[0] == '0' && str.size() >= 2 && str[1] == 'x'){
@@ -90,7 +65,6 @@ vector<int> convert_num(string str, int bytes, int zero_sign){
 		for (int index = 2 ; index < 9 ; index++){
 			string temp;
 			temp = temp + str[index];
-			//cout << temp << endl;
 			int l = (index == 8) ? 2:4;
 			for (int index = 0 ; index < l ; index++){
 				int num = binary_convert[temp][index] - '0';
@@ -105,8 +79,6 @@ vector<int> convert_num(string str, int bytes, int zero_sign){
 
     is >> num;
     vector<int> res;
-
-    //cout << num <<" " << bytes<< endl;
     int sign = (num >= 0 || zero_sign == 0)? 0:1;
     for (int index = 0 ; index < bytes ; index++){
     	res.push_back(sign);
@@ -119,7 +91,6 @@ vector<int> convert_num(string str, int bytes, int zero_sign){
 
     while (back != 0 && index < bytes){
     	if (back % 2 == 1){
-    		//cout << index << endl;
     		res[index] = 1;
     		num -= 1;
     	}else{
@@ -127,15 +98,8 @@ vector<int> convert_num(string str, int bytes, int zero_sign){
     	}
     	index++;
     	back /= 2;
-    //	cout << res[index] << endl;
     }
     reverse(res.begin(), res.end());
-    //cout << res.size() << endl;
-/*
-    for (int index = 0 ; index < res.size() ; index++)
-    	cout << res[index] ;
-    cout << endl;*/
-    //print_vec(res);
     return res;
 
 }
@@ -161,7 +125,6 @@ string pre_processing(string str){
     {
         str.replace(pos, 1, " ");
     }
-
     return str;
 }
 vector<int> parser(string str){
@@ -171,29 +134,18 @@ vector<int> parser(string str){
 	in >> code;
 	int final_mode = mode[code];
 	vector<int> opcode = convert_opcode(code,6);
-	//print_vec(opcode);
-	//cout << "--------" << opcode << endl;
 	string rd;
 	string rs;
 	string rt;
 	string imm;
 	string sa;
 	string addr;
-
 	vector<int> res(opcode.begin(), opcode.end());
-	//cout << "hello" << endl;
-	//res.insert(res.end(), opcode.begin(), opcode.end());
-	//cout << "hello" << endl;
 	if (final_mode == 0){
 		in >> rd >> rs >> rt;
 		vector<int>frd = convert_opcode(rd,5);
 		vector<int>frs = convert_opcode(rs,5);
 		vector<int>frt = convert_opcode(rt,5);
-/*
-    print_vec2(frs);
-    print_vec2(frt);
-    print_vec2(frd);
-    cout << endl;*/
 		res.insert(res.end(), frs.begin(), frs.end());
 		res.insert(res.end(), frt.begin(), frt.end());
 		res.insert(res.end(), frd.begin(), frd.end());
@@ -202,20 +154,9 @@ vector<int> parser(string str){
 	}
 	if (final_mode == 1){
 		in >> rt >> rs >> imm;
-		//cout << rt << " " << rs << " " << imm << endl;
-
 		vector<int> frt = convert_opcode(rt, 5);
 		vector<int> frs = convert_opcode(rs, 5);
-
 		vector<int> fimm = convert_num(imm, 16, 1);
-    /*print_vec2(frs);
-    print_vec2(frt);
-    print_vec2(fimm);
-    cout << endl;*/
-		//print_vec(fimm);
-		//print_vec(frt);
-		//print_vec(frs);
-		//print_vec(fimm);
 		res.insert(res.end(), frs.begin(), frs.end());
 		res.insert(res.end(), frt.begin(), frt.end());
 		res.insert(res.end(), fimm.begin(), fimm.end());
@@ -227,11 +168,6 @@ vector<int> parser(string str){
 		vector<int> frd = convert_opcode(rd, 5);
 		vector<int> frt = convert_opcode(rt, 5);
 		vector<int> fsa = convert_num(sa, 6, 0);
-/*
-    print_vec2(frt);
-    print_vec2(frd);
-    print_vec2(fsa);
-    cout << endl;*/
 		for (int index = 0 ; index < 5 ; index++)
 			res.push_back(0);
 		res.insert(res.end(), frt.begin(), frt.end());
@@ -245,11 +181,6 @@ vector<int> parser(string str){
 		vector<int> frs = convert_opcode(rs, 5);
 		vector<int> frt = convert_opcode(rt, 5);
 		vector<int> fimm = convert_num(imm, 16, 1);
-    /*
-    print_vec2(frs);
-    print_vec2(frt);
-    print_vec2(fimm);
-    cout << endl;*/
 		res.insert(res.end(), frs.begin(), frs.end());
 		res.insert(res.end(), frt.begin(), frt.end());
 		res.insert(res.end(), fimm.begin(), fimm.end());
@@ -259,11 +190,6 @@ vector<int> parser(string str){
         vector<int> frt = convert_opcode(rt, 5);
         vector<int> fimm = convert_num(imm, 16, 1);
         vector<int> frs = convert_opcode(rs, 5);
-        /*cout << rt << " " << imm << " -----" << rs << endl;
-        print_vec2(frs);
-        print_vec2(frt);
-        print_vec2(fimm);
-        cout << endl;*/
         res.insert(res.end(), frs.begin(), frs.end());
         res.insert(res.end(), frt.begin(), frt.end());
         res.insert(res.end(), fimm.begin(), fimm.end());
@@ -272,9 +198,6 @@ vector<int> parser(string str){
 		in >> rs >> imm;
 		vector<int> frs = convert_opcode(rs, 5);
 		vector<int> fimm = convert_num(imm, 16,1);
-    //print_vec2(frs);
-    //print_vec2(fimm);
-    //cout << endl;
 		res.insert(res.end(), frs.begin(), frs.end());
 		for (int index = 0 ; index < 5 ; index++)
 			res.push_back(0);
@@ -283,15 +206,11 @@ vector<int> parser(string str){
 	if (final_mode == 6){
 		in >> addr;
 		vector<int> faddr = convert_num(addr, 28, 1);
-    //print_vec2(faddr);
-    //cout << endl;
 		res.insert(res.end(), faddr.begin(), faddr.end());
 	}
 	if (final_mode == 7){
 		in >> rs;
 		vector<int> frs = convert_opcode(rs, 5);
-    //print_vec2(frs);
-    //cout << endl;
 		res.insert(res.end(), frs.begin(), frs.end());
 		for (int index = 0 ; index < 21 ; index++)
 			res.push_back(0);
@@ -310,8 +229,6 @@ int main(){
   for (int index = 0 ; index < 20 ; index++){
     	string str;
     	getline(file, str);
-    	//cout << str << endl;
-    	//cout << pre_processing(str) << endl;
     	print_vec(parser(pre_processing(str)));
  	}
   file.close();
